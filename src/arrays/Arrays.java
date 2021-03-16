df8f024fee77bc3cbc518115380c4b44864d7158
LoganHenning <lrhenning13@gmail.com>
2021-03-15 19:50:28 -0500

    First commit

---
+++ src/arrays/Arrays.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		int[] ages = new int[8];
+		ages[0] = 3;
+		ages[1] = 9;
+		ages[2] = 23;
+		ages[3] = 64;
+		ages[4] = 2;
+		ages[5] = 8;
+		ages[6] = 28;
+		ages[7] = 90;
+		
+		System.out.println(ages[ages.length - 1] - ages[0]);
+		
+	/*	for (int i = 0; i <= ages.length; i++) {
+			System.out.println(ages[i]);
+			*/
+		}
+	}
+
+
